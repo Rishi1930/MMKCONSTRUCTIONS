183ef3e59358e023e294eb10a38c360a44109ad7
BhanuprakashD8 <bbbuuunnn6@gmail.com>
2026-02-28 19:06:39 +0530

    send msg updated

--- script.js
+++ script.js
@@ -12,4 +12,35 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-console.log("JS Loaded");
+document.addEventListener("DOMContentLoaded", function(){
+
+  const form = document.getElementById("contactForm");
+
+  if(form){
+    form.addEventListener("submit", function(e){
+      e.preventDefault();
+
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const phone = document.getElementById("phone").value;
+      const message = document.getElementById("message").value;
+
+      const whatsappNumber = "919490539941";
+
+      const text =
+`New Contact Form Message:
+
+Name: ${name}
+Email: ${email}
+Phone: ${phone}
+Message: ${message}`;
+
+      const encodedText = encodeURIComponent(text);
+
+      const url = `https://wa.me/${whatsappNumber}?text=${encodedText}`;
+
+      window.open(url, "_blank");
+    });
+  }
+
+});
